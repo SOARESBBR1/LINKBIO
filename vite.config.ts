95dfa05644b0d383931e47d4adc45e904f9ab686
SOARESBBR1 <tauajoubert04@gmail.com>
2026-05-10 19:28:54 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173
+  },
   build: {
     outDir: "dist"
   }
